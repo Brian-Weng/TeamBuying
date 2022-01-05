95c51abbe7b6df7bead1a11bd7b40f61dded0da7
Bryant Weng <bryant52376@gmail.com>
2022-01-05 13:55:01 +0800

    【開發】完成Logger，TeamBuying的Method模板

---
+++ TeamBuying.DB/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamBuying.DB
+{
+    public class Logger
+    {
+        public static void WriteLog(Exception ex)
+        {
+            string msg =
+                $@"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}
+                   {ex.ToString()}
+
+                ";
+
+            System.IO.File.AppendAllText("D:\\Logs\\TeamBuyingLog.log", msg);
+            throw ex;
+        }
+    }
+}
---
+++ TeamBuying.DB/TeamBuyingManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamBuying.ORM.DBModels;
+
+namespace TeamBuying.DB
+{
+    public class TeamBuyingManager
+    {
+        public List<TeamBuying.ORM.DBModels.TeamBuying> GetTeamBuyings()
+        {
+            try
+            {
+                using (ContextModel context = new ContextModel())
+                {
+                    var obj = context.TeamBuyings.ToList();
+                    return obj;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLog(ex);
+                return null;
+            }
+        }
+
+        public void CreateTeamBuying(TeamBuying.ORM.DBModels.TeamBuying teamBuying)
+        {
+            try
+            {
+                using (ContextModel context = new ContextModel())
+                {
+                    teamBuying.CreateDate = DateTime.Now;
+                    teamBuying.Status = 0;
+                    context.TeamBuyings.Add(teamBuying);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLog(ex);
+                return;
+            }
+        }
+    }
+}
